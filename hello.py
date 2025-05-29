5ec2184a31b65417d294a4465b1f40410488f65e
Marco-IG <marcoibarragomez2@gmail.com>
2025-05-29 17:30:30 +0200

    (20/05/2025) 17:30

--- hello.py
+++ hello.py
@@ -1,1 +1,25 @@
-print("Hello World")
+print("Hello World") # Muestra un texto en la terminal
+
+import sys
+print(sys.version)
+
+"""
+Comentario uno
+"""
+
+x = 5 # Declara una variable
+x = "John" # Redeclara la variable
+
+print(x) # Mostrará el valor declarado por última vez antes del "print()"
+
+x = str(3)
+x = int(3) # El valor será una string (cadena de texto: "3")
+y = int(3) # El valor será un número entero (3)
+z = float(3) # El valor será un número decimal (3.0)
+
+print(type(x)) # Mostrará la clase de la X actual, declarada antes del print (En este caso será "int")
+
+# Las strings se pueden declarar con comillas dobles("") o simples('') 
+
+X = str(3) # Las variables son Case-Sensitive, es decir, es diferente una X de una x
+
